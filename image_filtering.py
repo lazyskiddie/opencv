6a3eb06a7e695345b88b3377eacf0a89509e16f6
lazyskiddie <sandeepsingh08550@gmail.com>
2026-04-25 21:39:56 +0530

    basic setups

--- image_filtering.py
+++ image_filtering.py
@@ -1,1 +1,13 @@
 import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
