38844d782702ba3d19a52f070d6bc1d51120f1b0
lazyskiddie <sandeepsingh08550@gmail.com>
2026-04-28 11:35:13 +0530

    i got little bit confusion about this wait in some time i will solve this problem

--- HDR/high_dynamic_range_image.py
+++ HDR/high_dynamic_range_image.py
@@ -1,4 +1,35 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
+def readImagesAndTimes():
+    filenames = ["img_0.033.jpg", "img_0.25.jpg", "img_2.5.jpg", "img_15.jpg"]
+    times = np.array([1 / 30.0, 0.25, 2.5, 15.0], dtype=np.float32)
+
+    images = []
+    for filename in filenames:
+        im = cv2.imread(filename)
+        images.append(im)
+
+    return images, times
+
+images, times = readImagesAndTimes()
+
+alignMTB = cv2.createAlignMTB()
+alignMTB.process(images, images)
+
+calibrateDebevec = cv2.createCalibrateDebevec()
+responseDebevec = calibrateDebevec.process(images, times)
+
+# Plot CRF
+x = np.arange(256, dtype=np.uint8)
+y = np.squeeze(responseDebevec)
+
+ax = plt.figure(figsize=(30, 10))
+plt.title("Debevec Inverse Camera Response Function", fontsize=24)
+plt.xlabel("Measured Pixel Value", fontsize=22)
+plt.ylabel("Calibrated Intensity", fontsize=22)
+plt.xlim([0, 260])
+plt.grid()
+plt.plot(x, y[:, 0], "b", x, y[:, 1], "g", x, y[:, 2], "r")
