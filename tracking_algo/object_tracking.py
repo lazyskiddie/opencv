ad8f5d786e7471f2a85d0d9b1cc2479b52c1635f
lazyskiddie <sandeepsingh08550@gmail.com>
2026-04-30 11:16:21 +0530

    i added what i understand about the GOTURN let see what come next

---
+++ tracking_algo/object_tracking.py
@@ -0,0 +1,19 @@
+import cv2
+
+# so i am going to use GOTURN tracking algo let see how it wok !!
+"""
+   main frame
+    _______
+   |       | \
+   |       |   \
+   |_______|    \
+                |---------|------------- |---------|
+    _______     |_________|              |         | predected frame
+   |       |   / neural network          |_________|
+   |       |  /
+   |_______| /
+  next frame
+
+
+
+"""
