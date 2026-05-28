4e45231208180dbf16545b659bf61fbb2bd48d22
lazyskiddie <sandeepsingh08550@gmail.com>
2026-05-28 21:20:01 +0530

    going to use mediapipe

--- handtracking/hand_tracking.py
+++ handtracking/hand_tracking.py
@@ -1,1 +1,11 @@
 import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    cv2.imshow('hand Detection', frame)
+    cv2.waitKey(1)
